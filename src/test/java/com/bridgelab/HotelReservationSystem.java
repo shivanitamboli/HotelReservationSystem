bbc6a5fa4402e3b733bb4b05890edbe57085663d
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-10-28 17:51:42 +0530

    Added : Find cheapest hotel in list

--- src/test/java/com/bridgelab/HotelReservationSystem.java
+++ src/test/java/com/bridgelab/HotelReservationSystem.java
@@ -17,6 +17,23 @@ public class HotelReservationSystem {
 		System.out.println("Hotel added successfully..!");
 	}
 
+	public void findCheapest() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter date range.\nEnter Start Date : ");
+		String startDate = sc.nextLine();
+		System.out.print("Enter End Date : ");
+		String endDate = sc.nextLine();
+		int min = hotels.get(0).getRate();
+		int ratingIndex = 0;
+		for (int i = 1; i < hotels.size(); i++) {
+			min = Math.min(min, hotels.get(i).getRate());
+			if (hotels.get(i).getRate() == min)
+				ratingIndex = i;
+		}
+		System.out.println("Best Cheapest Hotel : " + hotels.get(ratingIndex).getNameOfHotel());
+		System.out.println("Rate           : " + hotels.get(ratingIndex).getRate());
+	}
+
 	public void showHotels() {
 		System.out.println("Hotels ->");
 		System.out.printf("%-20s%-20s\n", "Name of hotel", "Rate");
@@ -34,7 +51,8 @@ public class HotelReservationSystem {
 		while (true) {
 			System.out.println("1. Add a New Hotel.");
 			System.out.println("2. Show Available Hotels.");
-			System.out.println("3. Exit From Hotel Reservation System.");
+			System.out.println("3. Find Cheapest Available Hotels.");
+			System.out.println("4. Exit From Hotel Reservation System.");
 			System.out.print("Enter your choice : ");
 			choice = sc.nextInt();
 			switch (choice) {
@@ -45,6 +63,9 @@ public class HotelReservationSystem {
 				h.showHotels();
 				break;
 			case 3:
+				h.findCheapest();
+				break;
+			case 4:
 				return;
 			default:
 				System.out.println("Wrong choice.");
